73532c06314b96e489028839e192a5d02ee49f42
Lei Nelissen <lei@codified.nl>
2022-06-16 13:35:31 +0200

    feat: Allow specific version to be specified when downloading postgres

--- packages/linux-ia32/scripts/download.js
+++ packages/linux-ia32/scripts/download.js
@@ -1,4 +1,4 @@
 const { downloadBinaries } = require('@embedded-postgres/downloader');
 const { readSymlinks } = require('@embedded-postgres/symlink-reader')
 const { version, os: [ platform ], cpu: [ arch ] } = require('../package.json');
-downloadBinaries(version, arch, platform).then(readSymlinks);
+downloadBinaries(process.argv[2] || version, arch, platform).then(readSymlinks);
