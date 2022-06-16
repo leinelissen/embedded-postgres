c5eda7d4f058ab3e0ec67275b9c8664d955dea1e
Lei Nelissen <lei@codified.nl>
2022-06-16 13:35:52 +0200

    fix: Make symlinks relative to their containing folder rather than the package root

--- packages/darwin-x64/scripts/hydrate-symlinks.js
+++ packages/darwin-x64/scripts/hydrate-symlinks.js
@@ -2,6 +2,7 @@ const fs = require('fs/promises');
 const path = require('path');
 
 const symlinkFile = path.join(process.cwd(), 'native', 'pg-symlinks.json');
+const originalWorkingDirectory = process.cwd();
 
 /**
  * This function will optionally read symlinks from `pg-symlinks.json` and
@@ -14,12 +15,26 @@ async function hydrateSymlinks() {
         .then(JSON.parse);
 
     // Re-hydrate all of them
-    await Promise.all(
-        symlinks.map(({ source, target }) => {
-            return fs.symlink(source, target);
-        })
-    // Swallow any errors, since the symlinks may already exist
-    ).catch(() => {});
+    for (let { source, target } of symlinks) {
+        // Make sure all symlinks are relative to the directory they are in, so
+        // that when whole directories are moved, the symlinks don't break as easily.
+        const dirname = path.dirname(source);
+        const relSource = path.relative(dirname, source);
+        const relTarget = path.relative(dirname, target);
+
+        // To make this work, we need to change the working directory for NodeJS
+        process.chdir(dirname);
+
+        try {
+            // We can then create the symlink
+            await fs.symlink(relSource, relTarget);
+        } catch {
+            // Swallow any errors, such as files already existing
+        }
+
+        // And switch back to the original working directory
+        process.chdir(originalWorkingDirectory);
+    }
 }
 
 hydrateSymlinks();
