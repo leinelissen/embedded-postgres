5cfb7f33835e545534eab741b7a7c59593e6370c
Lei Nelissen <lei@codified.nl>
2022-07-20 15:18:27 +0200

    feat: Also make package available for Windows and Linux

--- packages/embedded-postgres/src/binary.ts
+++ packages/embedded-postgres/src/binary.ts
@@ -18,10 +18,32 @@ function getBinaries(): Binaries {
                 case 'x64':
                     return require('@embedded-postgres/darwin-x64');
                 default:
-                    throw new Error('Unsupported arch: ' + arch);
+                    throw new Error(`Unsupported arch "${arch}" for platform "${platform}"`);
+            }
+        case 'linux':
+            switch(arch) {
+                case 'arm64':
+                    return require('@embedded-postgres/linux-arm64');
+                case 'arm':
+                    return require('@embedded-postgres/linux-arm');
+                case 'ia32':
+                    return require('@embedded-postgres/linux-ia32');
+                case 'ppc64':
+                    return require('@embedded-postgres/linux-ppc64');
+                case 'x64':
+                    return require('@embedded-postgres/linux-x64');
+                default:
+                    throw new Error(`Unsupported arch "${arch}" for platform "${platform}"`);    
+            }
+        case 'win32':
+            switch(arch) {
+                case 'x64':
+                    return require('@embedded-postgres/windows-x64');
+                default:
+                    throw new Error(`Unsupported arch "${arch}" for platform "${platform}"`);
             }
         default:
-            throw new Error('Unsupported platform: ' + platform);
+            throw new Error(`Unsupported platform "${platform}"`);
     }
 }
 
