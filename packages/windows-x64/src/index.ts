3fe1731b580648ff57eca28be1d0e003aa3d4be0
Lei Nelissen <lei@codified.nl>
2022-07-22 15:15:24 +0200

    fix: append .exe to windows binaries

--- packages/windows-x64/src/index.ts
+++ packages/windows-x64/src/index.ts
@@ -1,6 +1,6 @@
 import path from 'path';
 
-export const pg_ctl = path.resolve(__dirname, '..', 'native', 'bin', 'pg_ctl');
-export const initdb = path.resolve(__dirname, '..', 'native', 'bin', 'initdb');
-export const postgres = path.resolve(__dirname, '..', 'native', 'bin', 'postgres');
+export const pg_ctl = path.resolve(__dirname, '..', 'native', 'bin', 'pg_ctl.exe');
+export const initdb = path.resolve(__dirname, '..', 'native', 'bin', 'initdb.exe');
+export const postgres = path.resolve(__dirname, '..', 'native', 'bin', 'postgres.exe');
 
