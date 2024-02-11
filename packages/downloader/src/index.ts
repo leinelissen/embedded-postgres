2493d2995e0a37d3cc0917c45d84a0ad173515aa
Lei Nelissen <lei@codified.nl>
2024-02-11 16:48:05 +0100

    fix: enable binary downloads on win32

--- packages/downloader/src/index.ts
+++ packages/downloader/src/index.ts
@@ -2,9 +2,16 @@ import AdmZip from 'adm-zip';
 import fs from 'fs/promises';
 import { spawnSync } from 'child_process';
 import { coerce } from 'semver';
+import os from 'os';
 
 type AcceptedArchs = 'amd64' | 'arm64v8' | 'arm32v6' | 'arm32v7' | 'i386' | 'ppc64le';
 type AcceptedPlatforms = 'darwin' | 'linux' | 'windows';
+
+// Specify the command which should be used to check whether a command is 
+// available on the current system
+const whichCommand = os.platform() === 'win32'
+    ? 'where'
+    : 'which';
 
 /**
  * Map the output from os.arch to an architecture that is supported by the
@@ -45,6 +52,17 @@ function mapPlatform(platform: string): AcceptedPlatforms {
 }
 
 /**
+ * Check whether one or more binaries are available on the current system
+ */
+function hasBinary(bin: string | string[]): boolean {
+    return (Array.isArray(bin) ? bin : [bin])
+        .every((name) => {
+            const output = spawnSync(whichCommand, [name], { stdio: 'inherit', shell: true });
+            return output.status === 0;
+        });
+}
+
+/**
  * Download the Postgresql binaries for a combination of version, architecture
  * and platform.
  * 
@@ -56,8 +74,8 @@ export async function downloadBinaries(version: string, arch: string, platform: 
     // GUARD: We'll only download mismatching binaries with the current system
     // arch and platform if the "--all" flag is supplied
     if (!process.argv.includes('--all') 
-        && arch.toString() !== process.arch
-        && platform.toString() !== process.platform
+        && (arch.toString() !== process.arch
+        || platform.toString() !== process.platform)
     ) {
         console.log(`Skipping download for ${platform}-${arch}, because it does not match the local system (${process.platform}-${process.arch}). If you wish to download all binaries, re-run this command with the "--all" flag.`);
         return false;
@@ -94,8 +112,51 @@ export async function downloadBinaries(version: string, arch: string, platform: 
     await fs.mkdir('native').catch(() => '');
     await fs.writeFile('native.txz', data);
 
-    // Then extract the file with tar
-    spawnSync('tar', ['xvf', 'native.txz', '-C', 'native'], { stdio: 'inherit' });
+    // Then extract the file with either tar, if it's available...
+    // NOTE: we cannot use tar on windows because it will fuck up the symlinks
+    if (os.platform() !== 'win32' && hasBinary(['tar', 'xz'])) {
+        // Call xz first, because tar on Windows hangs if it has to call a decompressor externally
+        const xzOutput = spawnSync('xz', ['-d', 'native.txz'], { stdio: 'inherit' });
+
+        // GUARD: Check that the output is satisfactory
+        if (xzOutput.status !== 0) {
+            console.error(xzOutput.output);
+            throw new Error('Failed to decompress tar with binaries...');
+        }
+
+        // Call tar
+        const tarOutput = spawnSync('tar', ['xvf', 'native.txz', '-C', 'native'], { stdio: 'inherit' });
+        
+        // GUARD: Check that the output is satisfactory
+        if (tarOutput.status !== 0) {
+            console.error(tarOutput.output);
+            throw new Error('Failed to tar with binaries...');
+        }
+    } else if (hasBinary(['7z'])) {
+        // Call 7zip first, because tar on Windows hangs if it has to call a decompressor externally
+        const xzOutput = spawnSync('7z', ['x', 'native.txz', '-aoa'], { stdio: 'inherit', shell: true });
+
+        // GUARD: Check that the output is satisfactory
+        if (xzOutput.status !== 0) {
+            console.error(xzOutput.output);
+            throw new Error('Failed to decompress tar with binaries...');
+        }
+
+        // Call tar
+        const tarOutput = spawnSync('7z', ['x', 'native.tar', '-o"native"', '-aoa'], { stdio: 'inherit', shell: true });
+        
+        // GUARD: Check that the output is satisfactory
+        if (tarOutput.status !== 0) {
+            console.error(tarOutput.output);
+            throw new Error('Failed to extract tar with binaries...');
+        }
+    } else {
+        // Abort to the user when unpacking utilities are missing
+        throw new Error('Failed to unpack as system packages are missing. Please install both the "tar" and "xz" / "zstd" utils');
+    }
+
+    // Remove the file
+    await fs.unlink('native.tar');
     await fs.unlink('native.txz');
 
     return true;
