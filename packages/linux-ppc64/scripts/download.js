bf9882407d176976898ed5113f721586a8a0a681
Lei Nelissen <lei@codified.nl>
2024-02-11 14:35:14 +0100

    fix: replace "with" syntax for "assert" syntax for broader node compatibility
    "with" isn't supported until Node v20.10.0

--- packages/linux-ppc64/scripts/download.js
+++ packages/linux-ppc64/scripts/download.js
@@ -1,5 +1,5 @@
 import { downloadBinaries } from '@embedded-postgres/downloader';
 import { readSymlinks } from '@embedded-postgres/symlink-reader';
-import packageJson from '../package.json' with { type: "json" };
+import packageJson from '../package.json' assert { type: "json" };
 const { version, cpu, os } = packageJson;
 downloadBinaries(process.argv[2] || version, cpu, os).then(readSymlinks);
